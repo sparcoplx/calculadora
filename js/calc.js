8b8bd30c82beb49275a3adc4710923a94fc7748d
sparcoplx <felix.vilches@mayor.cl>
2022-02-03 02:21:33 -0300

    final

--- js/calc.js
+++ js/calc.js
@@ -12,102 +12,96 @@ var sum = document.getElementById('b+');
 var rest = document.getElementById('b-');
 var mult = document.getElementById('bx');
 var div = document.getElementById('bd');
+var punto = document.getElementById('punto');
 var igual = document.getElementById('bigual');
 var resultado = document.getElementById('resultado');
+var borrar = document.getElementById('clr');
+var reset = document.getElementById('rst');
 
 boton0.onclick = function(e){
-        if (resultado=='0'){
+        if (resultado.value=='0'){
             resultado.value =  this.value;
         }else{
             resultado.value +=  this.value;
         }
  }
  boton1.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton2.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton3.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton4.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton5.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton6.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton7.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton8.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 boton9.onclick = function(e){
-    if (resultado==0){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
     }
 }
 sum.onclick = function(e){
-    if (resultado==0){
-        resultado.value =  this.value;
-    }else{
         resultado.value +=  this.value;
-    }
 }
 rest.onclick = function(e){
-    if (resultado==0){
-        resultado.value =  this.value;
-    }else{
         resultado.value +=  this.value;
-    }
 }
 mult.onclick = function(e){
-    if (resultado==0){
-        resultado.value =  this.value;
-    }else{
         resultado.value +=  this.value;
-    }
 }
 div.onclick = function(e){
-    if (resultado==0){
+        resultado.value +=  this.value;
+}
+punto.onclick = function(e){
+    if (resultado.value==0){
         resultado.value =  this.value;
     }else{
         resultado.value +=  this.value;
@@ -115,4 +109,10 @@ div.onclick = function(e){
 }
 igual.onclick= function (e){
     resultado.value = eval(resultado.value);
+}
+borrar.onclick = function (e){
+    resultado.value = resultado.value.substring(0, resultado.value.length - 1);
+}
+reset.onclick = function (e){
+    resultado.value = "";
 }
